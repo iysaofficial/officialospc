119c54f7776b7dbb6e35fe0420dff1a2ff97e261
unknown <youngscientist.iysa@gmail.com>
2024-01-12 17:15:33 +0700

    json add

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages";
 import Faq from "./pages/faq.jsx";
-import CompFaq from "./component/CompFaq.jsx"
 
 const router = createBrowserRouter([
   {
@@ -13,11 +12,7 @@ const router = createBrowserRouter([
   {
     path: "/faq",
     element: <Faq />
-  },
-  {
-    path: "/faq2",
-    element: <CompFaq />
-  },
+  }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
