2c5b1525c50fa3b917aeebd0344984ea8a6a8b62
iysaofficial <110337759+iysaofficial@users.noreply.github.com>
2025-03-13 14:01:25 +0700

    Update mainhero.jsx

--- src/component/mainhero.jsx
+++ src/component/mainhero.jsx
@@ -10,12 +10,12 @@ const mainhero = () => {
                         <div className="hero-wrapper row">
                             <div className="content-text col-lg-6">
                                 <div>
-                                    <h1 className="fw-weight-800">Coming Soon 2026</h1>
+                                    <h1 className="fw-weight-800">Segera Hadir 2026</h1>
                                     <hr style={{ border: '3px solid' }} />
                                     <p>Ikuti OSPC 2026 dan jadilah bagian dari peneliti muda Indonesia yang berkompetisi di tingkat dunia di event-event IYSA!</p>
                                 </div>
                                 <div className="mt-2">
-                                    <a href="/" className="btn btn-primary btn-action">Tutup Pendaftaran</a>
+                                    <a href="/" className="btn btn-primary btn-action">Segera Hadir</a>
                                 </div>
                             </div>
                             <div className="content-img col-lg-6">
